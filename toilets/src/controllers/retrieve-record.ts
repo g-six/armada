9965bc3224601622ed998ef1806940a236115e52
G6 <g6@mini.local>
2021-06-30 12:52:38 +0800

    chore: merge updates from main armada

--- toilets/src/controllers/retrieve-record.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response } from 'express'
-import { retrieveRecords } from '../models'
-
-export default async (req: Request, res: Response) => {
-    const docs = await retrieveRecords()
-    return res.status(200).json({ docs })
-}
