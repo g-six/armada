dbadd941be93b7ad9ce7817613b232044f6d8b62
Rey <gerard@nerubia.com>
2022-05-15 03:58:26 +0000

    ci: 🎡 github actions

--- stacks/ApiAuthStack.ts
+++ stacks/ApiAuthStack.ts
@@ -1,26 +1,27 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import { DomainName } from '@aws-cdk/aws-apigatewayv2-alpha'
 import { StackContext, Api } from '@serverless-stack/resources'
 import { domain_props } from './domain-config'
 
 export function ApiAuthStack(this: any, { stack }: StackContext) {
-  const api = new Api(stack, "auth", {
-    customDomain: {
-      cdk: {
-        domainName: DomainName.fromDomainNameAttributes(
-          this,
-          'ApiDomain',
-          domain_props,
-        ),
-      },
-      path: 'auth',
-    },
-    routes: {
-      "POST /signup": 'services/auth/controllers/main.signUp',
-    },
-  });
+    const api = new Api(stack, 'auth', {
+        customDomain: {
+            cdk: {
+                domainName: DomainName.fromDomainNameAttributes(
+                    this,
+                    'ApiDomain',
+                    domain_props,
+                ),
+            },
+            path: 'auth',
+        },
+        routes: {
+            'POST /signup': 'services/auth/controllers/main.signUp',
+        },
+    })
 
-  // Show the API endpoint in the output
-  this.addOutputs({
-    ApiEndpoint: api.url,
-  })
+    // Show the API endpoint in the output
+    this.addOutputs({
+        ApiEndpoint: api.url,
+    })
 }
--- stacks/MyStack.ts
+++ stacks/MyStack.ts
@@ -1,14 +1,14 @@
-import { StackContext, Api } from "@serverless-stack/resources";
+import { StackContext, Api } from '@serverless-stack/resources'
 
 export function MyStack({ stack }: StackContext) {
-  const api = new Api(stack, "api", {
-    routes: {
-      "GET /": "functions/lambda.handler",
-    },
-  });
+    const api = new Api(stack, 'api', {
+        routes: {
+            'GET /': 'functions/lambda.handler',
+        },
+    })
 
-  // Show the API endpoint in the output
-  this.addOutputs({
-    ApiEndpoint: api.url,
-  })
+    // Show the API endpoint in the output
+    this.addOutputs({
+        ApiEndpoint: api.url,
+    })
 }
