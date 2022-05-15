58e7c7ef3c01e3c4e953a5a9d384eef65064317d
Rey <gerard@nerubia.com>
2022-05-15 20:21:40 +0000

    fix: 🐛 cases where link has expired

--- src/services/auth/controllers/main.ts
+++ src/services/auth/controllers/main.ts
@@ -16,7 +16,7 @@ export const activate: APIGatewayProxyHandlerV2 = async (event) => {
         statusCode: 400
     }
     try {
-        const { t: token } = event.queryStringParameters
+        const { t: token, redirect } = event.queryStringParameters
         const data = decode(token) as JwtPayload
         const expires = new Intl.DateTimeFormat(undefined, {
             day: '2-digit',
@@ -27,17 +27,31 @@ export const activate: APIGatewayProxyHandlerV2 = async (event) => {
             timeZone: 'America/Vancouver',
             timeZoneName: 'short',
         }).format(new Date(data.expires_at))
+
+        let headers
+        let status = 301
         if (data.expires_at < Date.now()) {
+            headers = { Location: decodeURIComponent(redirect + 'auth/signup?msg=ACTIVATION_LINK_EXPIRED') }
             return toErrorResponse({
                 name: 'token',
                 message: 'TOKEN_HAS_EXPIRED',
                 type: ResponseErrorTypes.InvalidToken,
-            }, 301, { Location: 'https://app.50stacksofgrey.com/auth/request-activation' })
+            }, status, headers)
+        }
+        const results = await model.activate(data.id)
+
+        if (redirect) {
+            if (results) {
+                status = 302
+                headers = { Location: decodeURIComponent(redirect + 'auth/login?msg=ACTIVATED') }
+            } else {
+                headers = { Location: decodeURIComponent(redirect + 'auth/signup?msg=ACTIVATION_LINK_EXPIRED') }
+            }
         }
         return toSuccessResponse({
             data,
             expires,
-        })
+        }, status, headers)
     } catch (e) {
         return toErrorResponse(e as Record<string, unknown>)
     }
@@ -53,6 +67,7 @@ export const signUp: APIGatewayProxyHandlerV2 = async (event) => {
         )
 
         const record = await model.signUp(email, password)
+        const exception = record as { error: string }
         const created = record as AdminCreateUserCommandOutput
         const existing = record as AdminGetUserCommandOutput
         let user_id
@@ -64,7 +79,7 @@ export const signUp: APIGatewayProxyHandlerV2 = async (event) => {
         } else {
             return toErrorResponse({
                 type: ResponseErrorTypes.Unauthorized,
-                message: 'Error in sign up.'
+                message: exception.error || 'Error in sign up.',
             })
         }
 
@@ -72,7 +87,7 @@ export const signUp: APIGatewayProxyHandlerV2 = async (event) => {
             id: user_id,
             issued_at: Date.now(),
             created_at: Date.now(),
-            expires_at: Date.now() + (15 * 60 * 1000),
+            expires_at: Date.now() + (10 * 60 * 1000),
         }
 
         const algorithm: TAlgorithm = 'HS512'
@@ -83,7 +98,13 @@ export const signUp: APIGatewayProxyHandlerV2 = async (event) => {
         )
 
         SGMail.setApiKey(config.ARMADA_SENDGRID_API_KEY)
-        const url = `https://${config.ARMADA_CUSTOM_DOMAIN}/auth/activate?t=${activation_token}`
+        let url = `https://${config.ARMADA_CUSTOM_DOMAIN}/auth/activate?`
+        if (event.headers.referer) {
+            url = `${url}redirect=${encodeURIComponent(event.headers.referer)}`
+            console.log(url)
+        }
+        url = `${url}&t=${activation_token}`
+
         await sendTemplate(
             'd-3ec759e22ea24af9803a7a7420d6e222',
             'Account Activation',
