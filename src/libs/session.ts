7cf688e31ed1f1da441c74af3c390973c070eb95
Rey <gerard@nerubia.com>
2022-05-16 00:03:09 +0000

    feat: 🎸 coggnito secret hashing util

--- src/libs/session.ts
+++ src/libs/session.ts
@@ -1,6 +1,16 @@
+import * as crypto from 'crypto-js'
+import { config } from 'generics/config'
 export interface Session {
     id: string
     created_at: number
     issued_at: number
     expires_at: number
+}
+export function hashCognitoSecret(username: string): string {
+    return crypto.enc.Base64.stringify(
+        crypto.HmacSHA256(
+            username + config.ARMADA_COGNITO_CLIENT_ID,
+            crypto.enc.Utf8.parse(config.ARMADA_COGNITO_CLIENT_SECRET ?? ''),
+        ),
+    )
 }
